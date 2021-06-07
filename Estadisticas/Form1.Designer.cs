172c46bfe500e171abab41eea9892d88f4d76028
MonsalveR <felipe.monsalve851@pascualbravo.edu.co>
2021-06-07 14:09:52 -0500

    Primer Proyecto

---
+++ Estadisticas/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Estadisticas
+{
+    partial class frmEstadisticas
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxMes = new System.Windows.Forms.ComboBox();
+            this.cbxProdcuto = new System.Windows.Forms.ComboBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxMes
+            // 
+            this.cbxMes.FormattingEnabled = true;
+            this.cbxMes.Items.AddRange(new object[] {
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio"});
+            this.cbxMes.Location = new System.Drawing.Point(12, 28);
+            this.cbxMes.Name = "cbxMes";
+            this.cbxMes.Size = new System.Drawing.Size(121, 23);
+            this.cbxMes.TabIndex = 0;
+            // 
+            // cbxProdcuto
+            // 
+            this.cbxProdcuto.FormattingEnabled = true;
+            this.cbxProdcuto.Items.AddRange(new object[] {
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio"});
+            this.cbxProdcuto.Location = new System.Drawing.Point(174, 28);
+            this.cbxProdcuto.Name = "cbxProdcuto";
+            this.cbxProdcuto.Size = new System.Drawing.Size(121, 23);
+            this.cbxProdcuto.TabIndex = 1;
+            this.cbxProdcuto.Visible = false;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(407, 27);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(22, 114);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.RowTemplate.Height = 25;
+            this.dgvDatos.Size = new System.Drawing.Size(470, 150);
+            this.dgvDatos.TabIndex = 3;
+            this.dgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Location = new System.Drawing.Point(12, 294);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(96, 23);
+            this.btnImportar.TabIndex = 4;
+            this.btnImportar.Text = "Importar xlsx";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.btnImportar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(137, 294);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(96, 23);
+            this.btnGuardar.TabIndex = 5;
+            this.btnGuardar.Text = "Guardar xlsx";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(272, 294);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 6;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // frmEstadisticas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 338);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnImportar);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cbxProdcuto);
+            this.Controls.Add(this.cbxMes);
+            this.Name = "frmEstadisticas";
+            this.Text = "Estadisticas";
+            this.Load += new System.EventHandler(this.frmEstadisticas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxMes;
+        private System.Windows.Forms.ComboBox cbxProdcuto;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+    }
+}
+
---
+++ Estadisticas/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CSharp;
+using SpreadsheetLight;
+
+namespace Estadisticas
+{
+    public partial class frmEstadisticas : Form
+    {
+        //private string path = @"C:\Users\carlo\source\repos\Archivos.xlsx";
+        public frmEstadisticas()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnImportar_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path;
+                path = openFileDialog1.FileName;
+                SLDocument sl = new SLDocument(path);
+                int row = 2;
+                List<Producto> listProducto = new List<Producto>();
+                while (!string.IsNullOrEmpty(sl.GetCellValueAsString(row, 1)))
+                {
+                    Producto producto = new Producto();
+                    producto.Codigo = sl.GetCellValueAsString(row, 1);
+                    producto.Nombre = sl.GetCellValueAsString(row, 2);
+                    producto.Precio = sl.GetCellValueAsInt32(row, 3);
+                    producto.Cantidad = sl.GetCellValueAsInt32(row, 4);
+
+                    listProducto.Add(producto);
+
+                    row += 1;
+                }
+
+                dgvDatos.DataSource = listProducto;
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path;
+                SLDocument sl = new SLDocument();
+                int iR = 2; int iC = 1;
+                foreach (DataGridViewColumn clum in dgvDatos.Columns)
+                {
+                    sl.SetCellValue(1, iC, clum.HeaderText.ToString());
+                    iC += 1;
+                }
+                foreach (DataGridViewRow row in dgvDatos.Rows)
+                {
+                    sl.SetCellValue(iR, 1, row.Cells[0].Value.ToString());
+                    sl.SetCellValue(iR, 2, row.Cells[1].Value.ToString());
+                    sl.SetCellValue(iR, 3, row.Cells[2].Value.ToString());
+                    sl.SetCellValue(iR, 4, row.Cells[3].Value.ToString());
+                    iR += 1;
+                }
+                path = saveFileDialog1.FileName;
+                sl.SaveAs(path);
+            }
+           
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            SLDocument sl = new SLDocument(@"C:\Users\carlo\source\repos\Archivos.xlsx");
+            int row = 2;
+            List<Producto> listProducto = new List<Producto>();
+            while (!string.IsNullOrEmpty(sl.GetCellValueAsString(row, 1)))
+            {
+                Producto producto = new Producto();
+                producto.Codigo = sl.GetCellValueAsString(row, 1);
+                producto.Nombre = sl.GetCellValueAsString(row, 2);
+                producto.Precio = sl.GetCellValueAsInt32(row, 3);
+                producto.Cantidad = sl.GetCellValueAsInt32(row, 4);
+
+                listProducto.Add(producto);
+
+                row += 1;
+            }
+            int rows;
+            int iR = 1;
+            rows = dgvDatos.CurrentRow.Index;
+            listProducto.Clear();
+            while (!string.IsNullOrEmpty(sl.GetCellValueAsString(iR, 1)))
+            {
+                Producto producto = new Producto();
+                if (rows == iR)
+                {
+                    iR += 1;
+                }else
+                {
+                    producto.Codigo = sl.GetCellValueAsString(iR, 1);
+                    producto.Nombre = sl.GetCellValueAsString(iR, 2);
+                    producto.Precio = sl.GetCellValueAsInt32(iR, 3);
+                    producto.Cantidad = sl.GetCellValueAsInt32(iR, 4);
+
+                    listProducto.Add(producto);
+
+                    iR += 1;
+                }
+            }
+            dgvDatos.DataSource = listProducto;
+        }
+        private void frmEstadisticas_Load(object sender, EventArgs e)
+        {
+            SLDocument sl = new SLDocument(@"C:\Users\carlo\source\repos\Archivos.xlsx");
+            int row = 2;
+            List<Producto> listProducto = new List<Producto>();
+            while (!string.IsNullOrEmpty(sl.GetCellValueAsString(row, 1)))
+            {
+                Producto producto = new Producto();
+                producto.Codigo = sl.GetCellValueAsString(row, 1);
+                producto.Nombre = sl.GetCellValueAsString(row, 2);
+                producto.Precio = sl.GetCellValueAsInt32(row, 3);
+                producto.Cantidad = sl.GetCellValueAsInt32(row, 4);
+
+                listProducto.Add(producto);
+
+                row += 1;
+            }
+
+            dgvDatos.DataSource = listProducto;
+        }
+    }
+}
